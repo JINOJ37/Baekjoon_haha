75fd6cd9719f7a4c6a4f0c25a59d4849f198a09d
JINOJ37 <127945095+JINOJ37@users.noreply.github.com>
2023-09-05 17:36:29 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,7 @@
+grade = int(input())
+if 0<=grade<=100:
+    if 90<=grade<=100: print("A")
+    elif 80<=grade: print("B")
+    elif 70<=grade: print("C")
+    elif 60<=grade: print("D")
+    else: print("F")
