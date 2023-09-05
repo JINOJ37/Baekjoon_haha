e0cd43949836a8240ee734ea9e02753ada2b3a60
JINOJ37 <127945095+JINOJ37@users.noreply.github.com>
2023-09-05 17:57:19 +0900

    [Bronze V] Title: 사분면 고르기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,8 @@
+x = int(input())
+y = int(input())
+
+if -1000<=x<=1000 and -1000<=y<=1000 and (x,y)!=0:
+    if x>0 and y>0: print("1")
+    elif x<0 and y>0: print("2")
+    elif x<0 and y<0: print("3")
+    elif x>0 and y<0: print("4")
