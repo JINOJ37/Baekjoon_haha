ec4d528326209b0e33b33e7d1395e143196698ac
JINOJ37 <127945095+JINOJ37@users.noreply.github.com>
2023-09-07 22:33:27 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,13 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+list = [i+1 for i in range(n)]
+
+for _ in range(m):
+    i, j = map(int, sys.stdin.readline().split())
+    k = list[i-1]
+    list[i-1] = list[j-1]
+    list[j-1] = k
+
+for i in list:
+    print(i, end=" ")
