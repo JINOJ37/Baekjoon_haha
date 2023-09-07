28148726ddc30e3e30de4c71feee71037cf365f9
JINOJ37 <127945095+JINOJ37@users.noreply.github.com>
2023-09-08 02:47:59 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -6,8 +6,7 @@ list = [i+1 for i in range(n)]
 for _ in range(m):
     i, j = map(int, sys.stdin.readline().split())
     temp = list[i-1:j][::-1]
-    for k in range(i-1,j):
-        list[k] = temp.pop(0)
+    list[i-1:j] = temp
 
 for i in list:
     print(i, end=" ")
