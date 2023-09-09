e21aa3246bbedb77769c6bf2e61461508ccd4c6f
JINOJ37 <127945095+JINOJ37@users.noreply.github.com>
2023-09-09 17:43:20 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -0,0 +1,23 @@
+c = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+
+s = input()
+check = ""
+number = 0
+
+for i in s:
+    check += i
+    if len(check) == 1 or check == 'dz':
+        continue
+    else:
+        if check in c:
+            number += 1
+            check = ""
+        elif len(check) == 2:
+            check = check[1:]
+            number += 1
+        else:
+            check = check[2:]
+            number += 2
+
+number += len(check)
+print(number)
