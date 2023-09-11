e1e8ea43dad159b63249cc3b5515cbdd4b639aed
JINOJ37 <127945095+JINOJ37@users.noreply.github.com>
2023-09-11 18:27:46 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -0,0 +1,13 @@
+n, b = input().split()
+n = n[::-1]
+b = int(b)
+result = 0
+
+for i in range(len(n)):
+    try:
+        result += int(n[i])*(b**i)
+    except:
+        result += (ord(n[i])-55)*(b**i)
+        continue
+
+print(result)
